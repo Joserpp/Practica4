b45544e78d13291916e5ea79b56e5ec9cdd58e7f
alonsodm12 <alonsodmx@gmail.com>
2022-12-12 17:26:07 +0100

    p
    Co-authored-by: Joserpp <Joserpp@users.noreply.github.com>

---
+++ ej04_juego_letras/estudiante/src/cantidad_letras.cpp
@@ -0,0 +1,19 @@
+#include <fstream>
+#include <iostream>
+
+#include "dictionary.h"
+#include "LettersSet.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    if(argc != 3){
+        cerr << "Los parametros son: " << endl;
+        cerr << "1.- El fichero con el diccionario" << endl;
+        cerr << "2.- El fichero con letras" << endl;
+        return EXIT_FAILURE;
+    }
+
+    
+}
