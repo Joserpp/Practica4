9a25244b390d78624a87069195eeb727f8d37b8c
alonsodm12 <alonsodmx@gmail.com>
2022-12-22 13:46:04 +0100

    p

---
+++ ej04_juego_letras/estudiante/include/solver.h
@@ -0,0 +1,34 @@
+#include <string>
+#include <vector>
+#include <utility>
+#include <set>
+#include "dictionary.h"
+#include <cmath>
+#include <letters_set.h>
+
+using namespace std;
+
+class Solver{
+
+    private:
+
+     Dictionary diccionario;
+
+     LetterSet conjunto_letras;
+ 
+
+    public:
+
+     Solver(const Dictionary & dict,const LetterSet & letters_set);
+
+     pair<vector<string>,int>getSolutions(const vector<char> & available_letters,bool score_game);
+
+    
+
+
+
+
+
+
+
+};
