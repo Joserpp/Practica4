87106451c48fad6d2cc5f66bf5f583af219dbf12
alonsodm12 <alonsodmx@gmail.com>
2022-12-23 16:46:05 +0100

    p Co-authored-by: Joserpp <Joserpp@users.noreply.github.com>

--- ej04_juego_letras/estudiante/src/partida_letras.cpp
+++ ej04_juego_letras/estudiante/src/partida_letras.cpp
@@ -7,18 +7,33 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-  if(argc != 2){
+  if(argc != 3){
     cout << "Los parametros son: " << endl;
     cout << "1.- El fichero con el diccionario" << endl;
+    cout << "2.- Contiene las letras y su puntuacion" << endl;
+    cout << "3.- Modo de juego (L = longitud, P = puntuación)" << endl;
+    cout << "4.- Cantidad de letras para la partida" << endl;
     return 1;
   }
 
   ifstream f_diccionario(argv[1]);
-
+  ifstream f_conjuntoletras(argv[2]);
+  
   if(!f_diccionario){
     cout << "No puedo abrir el fichero " << argv[1] << endl;
     return 0;
   }
+  
+  string cad="";
+  Dictionary diccionario;
+  LetterSet conjunto_letras;
+
+  while(f_diccionario >> cad)
+        diccionario.insert(cad);
+
+    f_conjuntoletras >> conjunto_letras;
+
+
 
   Dictionary diccionario;
   f_diccionario >> diccionario;
